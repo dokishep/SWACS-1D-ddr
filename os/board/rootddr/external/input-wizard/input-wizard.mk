c022b1b6aaf451d1ee84fe2cff274c461e3e1e07
Birch (gastown) <Birch@gastown.local>
2026-05-21 03:57:29 +0000

    Rework: fix SDL_GAMECONTROLLERCONFIG format, GUID, xinit, newlines in input-wizard
    - SDL_GAMECONTROLLERCONFIG: change dpup:Button9 format to dpup:bN (joystick index
      via JoystickButtonDown). write_config now writes guid,name,mapping 3-tuple format.
    - GUID: replace manual 13-byte format with guid.to_string() (SDL3 standard).
    - S10inputwiz: replace orphaned Xorg background with xinit to run wizard and
      auto-clean the X session.
    - Main.rs: remove unused ButtonPrompt.target_btn field and impl.
    - Cargo.toml, Config.in, input-wizard.mk, main.rs, S10inputwiz, input_wizard.sh:
      add trailing newlines.
    Fixes rework bead 6d7ca74e

---
+++ os/board/rootddr/external/input-wizard/input-wizard.mk
@@ -0,0 +1,24 @@
+INPUT_WIZARD_VERSION = 1.0.0
+INPUT_WIZARD_SITE = $(TOPDIR)/../../os/board/rootddr/external/input-wizard
+INPUT_WIZARD_SITE_METHOD = local
+
+INPUT_WIZARD_DEPENDENCIES = sdl3
+
+INPUT_WIZARD_CARGO_ENV = \
+    CARGO_HOME=$(HOST_DIR)/usr/share/cargo \
+    RUST_TARGET_PATH=$(HOST_DIR)/usr/share/rustc \
+    PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig:$(STAGING_DIR)/usr/share/pkgconfig \
+    SDL3_LIB=$(STAGING_DIR)/usr/lib \
+    SDL3_INCLUDE=$(STAGING_DIR)/usr/include/SDL3
+
+define INPUT_WIZARD_BUILD_CMDS
+    cd $(@D) && \
+    $(INPUT_WIZARD_CARGO_ENV) \
+    cargo build --release --target x86_64-unknown-linux-musl
+endef
+
+define INPUT_WIZARD_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/target/x86_64-unknown-linux-musl/release/input-wizard $(TARGET_DIR)/usr/local/bin/input-wizard
+endef
+
+$(eval $(cargo-package))
---
+++ os/board/rootddr/rootfs_overlay/etc/profile.d/input_wizard.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Input mapping profile - sourced at login
+# Populated by the input-mapping wizard on first boot
+export SDL_GAMECONTROLLERCONFIG=""
